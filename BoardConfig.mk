71cfe6c487d703d2d3b9b6a403d165cce8231eae
nicklovell23 <nicklovell23@gmail.com>
2015-05-10 12:36:21 -0500

    trltespr add board define for camera preview.
    Change-Id: Iaf24f78c0fbd14264cba5f3dc2de0701aadf00f8

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -20,6 +20,9 @@ TARGET_OTA_ASSERT_DEVICE := trltespr,trlteusc,SM-N910P,SM-N910R4
 # Kernel
 TARGET_KERNEL_VARIANT_CONFIG := apq8084_sec_trlte_spr_defconfig
 
+# Camera
+TARGET_FIXUP_PREVIEW := true
+
 # Radio
 BOARD_RIL_CLASS := ../../../device/samsung/trltespr/ril
 
